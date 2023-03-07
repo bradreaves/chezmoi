cd2ddc32959371cba1ac8d2dc3f97094368b72c7
Brad Reaves <bgr@2014-MBP.local>
2023-03-06 23:59:24 -0500

    Add path stuff and ls abbr to config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -30,10 +30,21 @@ abbr --add cpsshkey "cat ~/.ssh/id_ecdsa.pub | pbcopy"
 # Checking for OS: https://github.com/fish-shell/fish-shell/issues/8203
 # See also "fish-core-utils" plugin -- has a is:macos function (etc)
 
-
 ## Other aliases
 
 abbr --add cm "chezmoi"
 abbr --add cmcd "cd /Users/bgr/.local/share/chezmoi" # Go to chezmoi directory
+abbr --add cmst "chezmoi status" 
 
-abbr --add gofish "source ~/.config/fish/conf.d/*"
+abbr --add ll "ls -lh"
+abbr --add lll "ls -lah"
+abbr --add llll "ls -lRah"
+
+# Below is broken and does not work. I need to research how fish actually does
+# this. Or find a plugin.
+#abbr --add gofish "source ~/.config/fish/conf.d/*"
+
+## Path management
+
+fish_add_path "~/bin"
+
