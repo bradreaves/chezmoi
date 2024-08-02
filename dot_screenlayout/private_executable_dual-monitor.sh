179cf6bf0855933c59be9175b96d8da85718aa75
Brad Reaves <brad@reaves.email>
2024-08-01 21:22:35 -0400

    fix default screen layout

--- dot_screenlayout/private_executable_dual-monitor.sh
+++ dot_screenlayout/private_executable_dual-monitor.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --mode 3840x2160 --pos 0x0 --rotate normal --output DisplayPort-1 --primary --mode 3840x2160 --pos 3840x0 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --off
+xrandr --output DisplayPort-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DisplayPort-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --off
